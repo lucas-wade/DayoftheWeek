f3c151bcc8aed37fbb4f138690927b85382419f2
BuildTools <mjbrowningg@gmail.com>
2017-02-17 20:52:53 -0800

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	const timeFormat = "01/02/2006"
+	arg := os.Args[1]
+
+	t, _ := time.Parse(timeFormat, arg)
+	day := t.Weekday().String()
+	fmt.Printf("The Weekday for %v is %v\n", t, day)
+}
