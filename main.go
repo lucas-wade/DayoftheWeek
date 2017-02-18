e05eee450abc68502589e24a92b19bf8cf1e92fc
BuildTools <mjbrowningg@gmail.com>
2017-02-18 10:08:20 -0800

    format time in a new way.

--- main.go
+++ main.go
@@ -10,11 +10,11 @@ func main() {
 	const timeFormat = "01/02/2006"
 	arg := os.Args[1]
 	t, err := time.Parse(timeFormat, arg)
-	day := t.Weekday().String()
+	d := t.Weekday().String()
 	if err != nil {
 		fmt.Println("The entered date is not valid.")
 	} else {
-		fmt.Printf("The Weekday for %v %v %v is %v\n", t.Month(), t.Day(), t.Year(), day)
+		t.Format(timeFormat)
+		fmt.Printf("The Weekday for %v is %v\n", t.Format(timeFormat), d)
 	}
-
 }
