7e887f1d1bbdd97b28161506a614e2ee5b1a3786
BuildTools <mjbrowningg@gmail.com>
2017-02-17 21:53:22 -0800

    add error handling for incorrect date

--- main.go
+++ main.go
@@ -9,8 +9,12 @@ import (
 func main() {
 	const timeFormat = "01/02/2006"
 	arg := os.Args[1]
-	t, _ := time.Parse(timeFormat, arg)
+	t, err := time.Parse(timeFormat, arg)
 	day := t.Weekday().String()
+	if err != nil {
+		fmt.Println("The entered date is not valid.")
+	} else {
+		fmt.Printf("The Weekday for %v %v %v is %v\n", t.Month(), t.Day(), t.Year(), day)
+	}
 
-	fmt.Printf("The Weekday for %v %v %v is %v\n", t.Month(), t.Day(), t.Year(), day)
 }
