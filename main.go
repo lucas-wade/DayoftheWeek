b229fe5631ecfc632cb534eecd06185f04d8c1f6
BuildTools <mjbrowningg@gmail.com>
2017-02-17 21:34:58 -0800

    cleaned up returned text

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 func main() {
 	const timeFormat = "01/02/2006"
 	arg := os.Args[1]
-
 	t, _ := time.Parse(timeFormat, arg)
 	day := t.Weekday().String()
-	fmt.Printf("The Weekday for %v is %v\n", t, day)
+
+	fmt.Printf("The Weekday for %v %v %v is %v\n", t.Month(), t.Day(), t.Year(), day)
 }
